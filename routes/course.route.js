c68d68ca45ee0904585767c1000b7516e4bcfd11
Maimoon Hossain <ashikasitosh740@gmail.com>
2025-08-10 23:25:13 +0600

    course create route added

--- routes/course.route.js
+++ routes/course.route.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import upload from "../utils/multer.js";
+import { createCourse } from "../controllers/course.controller.js";
 
 const router = express.Router();
 
