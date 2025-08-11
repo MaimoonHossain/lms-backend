3533408cf203ac4b33fc8839eedf47149c7ef405
MaimoonHossain <ashikasitosh740@gmail.com>
2025-08-11 13:02:30 +0600

    course list api integrated

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -26,3 +26,22 @@ export const createCourse = async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 };
+
+export const getCreatorCourses = async (req, res) => {
+  try {
+    const courses = await Course.find({ creator: req.id }).populate(
+      "creator",
+      "name email"
+    );
+
+    if (!courses || courses.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No courses found for this creator" });
+    }
+    res.status(200).json(courses);
+  } catch (error) {
+    console.error("Error fetching courses:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
