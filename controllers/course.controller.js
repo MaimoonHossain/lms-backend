83774705eb56b556b037723b8e08b126156a3784
MaimoonHossain <ashikasitosh740@gmail.com>
2025-08-13 17:16:12 +0600

    edit and get coures by id api created

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -27,6 +27,49 @@ export const createCourse = async (req, res) => {
   }
 };
 
+export const editCourse = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, subTitle, description, category, level, thumbnail } =
+      req.body;
+    const course = await Course.findById(id);
+
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+
+    course.title = title;
+    course.subTitle = subTitle;
+    course.description = description;
+    course.category = category;
+    course.level = level;
+    course.thumbnail = thumbnail;
+
+    const updatedCourse = await course.save();
+    res.status(200).json(updatedCourse);
+  } catch (error) {
+    console.error("Error editing course:", error);
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const getCourseById = async (req, res) => {
+  try {
+    const course = await Course.findById(req.params.id).populate(
+      "creator",
+      "name email"
+    );
+
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+    res.status(200).json(course);
+  } catch (error) {
+    console.error("Error fetching course:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
 export const getCreatorCourses = async (req, res) => {
   try {
     const courses = await Course.find({ creator: req.id }).populate(
--- routes/course.route.js
+++ routes/course.route.js
@@ -4,12 +4,16 @@ import isAuthenticated from "../middlewares/isAuthenticated.js";
 import upload from "../utils/multer.js";
 import {
   createCourse,
+  editCourse,
+  getCourseById,
   getCreatorCourses,
 } from "../controllers/course.controller.js";
 
 const router = express.Router();
 
 router.route("/create").post(isAuthenticated, createCourse);
+router.route("/edit/:id").patch(isAuthenticated, editCourse);
+router.route("/get-course-by-id/:id").get(isAuthenticated, getCourseById);
 router.route("/").get(isAuthenticated, getCreatorCourses);
 
 export default router;
